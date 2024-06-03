6ace5d5da1b73809cdf508fb3c482bf2a429d47f
Lance Mattson <mattson24@gmail.com>
2024-06-03 16:24:00 -0500

    Initial commit

---
+++ src/main/java/com/java_react_task_manager/JavaReactTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.java_react_task_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaReactTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaReactTaskManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java_react_task_manager/JavaReactTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_react_task_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaReactTaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
